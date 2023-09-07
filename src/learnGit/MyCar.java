c05f39538fd98f3fe5646fe9e19c6b9bb9b8d512
PaariLenin <User@PKKDPC.lan>
2023-09-07 21:32:26 +1000

    Commit to Local repository 1

---
+++ src/learnGit/MyCar.java
@@ -0,0 +1,46 @@
+package learnGit;
+
+public class MyCar {
+
+	String carModel;
+	int carAge;
+	String carColor;
+	String carLength; // This is the length of the car
+	int carCost; //The car cost
+
+	void dueCare() {
+		System.out.println("\nCar Advice: \"Your car needs to serviced if more than 1 year old\"\n");
+	}
+
+	void maintenanceCost() {
+		System.out.println("Total maintenance on your car is about " + carAge * 400+ " for your car");
+	}
+
+	float carValue(int carCost) {
+		System.out.println("You bought it for "+ carCost);
+		float carValueNow;
+		if (carAge < 3) {
+			carValueNow = carCost*0.75f;
+			System.out.println("But now it costs about :" + carValueNow);
+		} else if (carAge <= 5) {
+			carValueNow = carCost*0.5f;
+			System.out.println("But now it costs about :" + carValueNow);
+		} else {
+			carValueNow = carCost * 0.25f;
+			System.out.println("But now it costs about :" + carValueNow);
+		}
+		return carValueNow;
+	}
+	
+	MyCar(){
+		System.out.println("Your car is created now");
+	};
+
+	MyCar(int i){
+		System.out.println("Your car is created now. But if you want more cars");
+	};
+	
+	MyCar(String i){
+		System.out.println("Your car is created now. But if you want more cars");
+	};
+}
---
+++ src/learnGit/MyCode.java
@@ -0,0 +1,31 @@
+package learnGit;
+import java.util.Scanner;
+
+public class MyCode {
+
+	public static void main(String[] args) {
+		MyCar mycar = new MyCar();
+		new MyCar(1);//Just to call a constructor of day with 1 parameter
+		try (Scanner getUserInput = new Scanner(System.in)) {
+			System.out.println("What your the model of your car ? ");
+			mycar.carModel = getUserInput.nextLine();
+			System.out.println("What your the Age of your car ? ");
+			mycar.carAge = getUserInput.nextInt();
+			System.out.println("How much did you pay for your car ? ");
+			mycar.carCost = getUserInput.nextInt();
+		} catch (Exception e) {
+		System.out.println("Oops! Its an invalid input from user : "+e);
+		}
+		
+		//Call all other classes I have created via other lessons
+	       carMethods(mycar); 
+	}
+	
+	private static void carMethods(MyCar inCar)
+	{
+		inCar.dueCare();
+		inCar.maintenanceCost();
+		inCar.carValue(inCar.carCost);
+	}
+}
+
